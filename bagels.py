f01ca7f5b199a098c3e88bded74de62230c905de
Prajjwal Kumar Upadhyay <prajjwal26082006@gmail.com>
2025-11-17 21:17:00 +0530

    added Bagles.py

---
+++ bagels.py
@@ -0,0 +1,46 @@
+import sys,random
+print('Bagels, a deductive logic game.')
+print('''i am thinking of a 3-digit number . Try to guess what it is.
+      
+      Here are some clue:
+      When i say :      That means:
+        Pico             One digit is correct but in the wrong position
+       Fermi             One position is correct but in the right position
+      Bagels             No digit is correct ''')
+SecretNumber=random.randint(100,999)
+Secretdigit=[]
+for d in str(SecretNumber):
+    Secretdigit.append(int(d))
+print('I have thought up a number.')
+print('You have 10 guesses to get it.')
+def result():
+    print("Guess the number or type 'exit' to end the Game.")
+    for i in range(1,11):
+        GuessedNumber=input()
+        GuessedDigit=[int(d) for d in str(GuessedNumber)]
+        if GuessedNumber.lower()=='exit':
+            sys.exit()
+        elif GuessedDigit==Secretdigit:
+            print(f'Fermi,Fermi,Fermi!,You have guessed the number in {i} guesses.')
+        if GuessedDigit[0] in Secretdigit:
+            if GuessedDigit[0]==Secretdigit[0]:
+                print('fermi')
+            else:
+                print('Pico')
+        if GuessedDigit[1] in Secretdigit:
+            if GuessedDigit[1]==Secretdigit[1]:
+                print('fermi')
+            else:
+                print('Pico')
+        if GuessedDigit[2] in Secretdigit:
+            if GuessedDigit[2]==Secretdigit[2]:
+                print('fermi')
+            else:
+                print('Pico')
+        if GuessedDigit not in Secretdigit:
+            print('Bagels')
+        if i>=10:
+            print('chances over:')
+            break
+    print(SecretNumber)
+print(result())
