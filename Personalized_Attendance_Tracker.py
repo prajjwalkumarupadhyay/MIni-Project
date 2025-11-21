69e8ec99e313c72fd1683158f8e9b36bf9a36c5d
Prajjwal Kumar Upadhyay <prajjwal26082006@gmail.com>
2025-11-21 21:59:27 +0530

    Added Personalized_Attendance_Tracker

---
+++ Personalized_Attendance_Tracker.py
@@ -0,0 +1,11 @@
+print('''This Program tracks and maintain your attendance
+      
+      What do you want to know :
+      1. Your Attendance percentage.
+      2. To know how many more holidays you can take.''')
+lectures={'monday':5,'tuesday':6,'wedneday':4,'thursday':6,'friday':5,'saturday':5}
+
+if 
+        
+    
+get_int_value=input()
