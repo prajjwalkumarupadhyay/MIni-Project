a1b872350467a590c41f50fee74c06c269e74bc0
PRAJJWAL <prajjwal26082006@gmail.com>
2025-11-24 09:08:04 +0530

    Add files via upload

---
+++ carrotinabox.py
@@ -0,0 +1,14 @@
+print('Carrot in the box by Prajjwal Kumar Upadhyay')
+print('--snip--')
+player1=input('Human player 1 , enter your name:')
+player2=input('Human player 2, enter your name:')
+print('HERE ARE TWO BOXES:')
+print(f''' 
+           ___________         ______________
+          /          /|       /             /|
+         +----------+ |      +-------------+ | 
+         |   RED    | |      |   GOLD      | |  
+         |   BOX    | /      |    BOX      | /
+         +----------+/       +-------------+/
+          {player1}             {player2}
+    ''')
