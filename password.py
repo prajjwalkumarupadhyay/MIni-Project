d235b1d0bdd62a0e1d6b8eac24f08c8a52c8a5e3
PRAJJWAL <prajjwal26082006@gmail.com>
2025-11-28 09:47:28 +0530

    Update password validation regex for strength check

--- password.py
+++ password.py
@@ -1,9 +1,10 @@
 import re              
 while True:
-   password=re.compile(r'\d+\w*{8,}',re.I)
+   password=re.compile(r'^(?=.*[0-9]).+$',re.I)
    output=password.search(input('Create your password \n'))
    if output is None:
       print('Please create a strong Password')
    else:
       print('Password Created! Your password is:',output.group())
+
       
