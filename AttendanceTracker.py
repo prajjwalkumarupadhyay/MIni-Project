0678c87c8cf100f30a13142b9b7260c6c312ea09
Prajjwal Kumar Upadhyay <prajjwal26082006@gmail.com>
2026-04-16 06:45:04 +0530

    Attendance Tracker

--- AttendanceTracker.py
+++ AttendanceTracker.py
@@ -1,7 +1,7 @@
 from  Personalized_Attendance_Tracker import Attendance # To Enter or Re Enter your attendance
 def Global():
     def Days():
-        day=[]
+        day=set()
         while True:
             value=input()
             if value.lower()=='d':
@@ -12,8 +12,13 @@ def Global():
             elif value.capitalize() not in lec:
                 print('Please enter valid input.')
                 continue
-            day.append(value.capitalize())
+            day.add(value.capitalize())
         return day
+    def Attendance():
+        class_attended=0
+        for i in present:
+            class_attended+=lec.get(i)
+        return class_attended
     lec={'Monday':5,
           'Tuesday':7,
           'Wednesday':5,
@@ -23,10 +28,11 @@ def Global():
     print("Enter on which day you were present and  Type (d)one when you have entered the value.")
     present=Days()
     print(present)
+    class_attended=Attendance()
+    print(class_attended)
 print('''Track Your Attendance:
       Do You want to Re-enter Your Attendace''')
 attendance=input()
 if attendance.lower()=='yes':
     print(Attendance())
 Global()
-    
