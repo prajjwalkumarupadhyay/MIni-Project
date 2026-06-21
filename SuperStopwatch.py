a7f401510eff293efbad10e4255d37a2e47a7938
Prajjwal Kumar Upadhyay <prajjwal26082006@gmail.com>
2026-06-21 14:11:54 +0530

    Super Stop Watch

---
+++ SuperStopwatch.py
@@ -0,0 +1,7 @@
+import time
+print('Press Enter to begin . Afterwards , press Enter to "click" the stopwatch . Press Command - C to quit. ')
+input()
+print('Started')
+startTime=time.time()
+lastTime=startTime
+lapNum=1
